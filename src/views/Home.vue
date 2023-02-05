9445dd3e79e4810fc665e992c90b7dd2666e738f
ksm0207 <ksm03071@naver.com>
2023-02-05 21:11:48 +0900

    230205 git test2

---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    Test : {{this.data1}}
+  </div>
+</template>
+
+<script lang="javascript">
+
+export default {
+  name : 'home',
+  data : () => ({
+      data1 : "Hello"
+  }),
+  methods : () =>({
+    getName : function (){
+      return ""
+    }
+  })
+}
+
+</script>
+
+<style scoped>
+
+</style>
