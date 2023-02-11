bc8dad26b12f380319d95c4e1aecb1fb1706ed8a
ksm0207 <ksm03071@naver.com>
2023-02-11 15:51:57 +0900

    230211 Test

---
+++ src/components/Container.vue
@@ -0,0 +1,32 @@
+<template>
+  <Post :instagram="this.instagram" />
+</template>
+
+<script>
+
+import Post from '../components/Post.vue'
+
+export default {
+  data() {
+    return {
+      return: {}
+    }
+  },
+  props : {
+    instagram : Object
+  },
+  components : {
+    Post
+  },
+  mounted() {
+
+  }
+
+
+}
+
+</script>
+
+<style scoped>
+
+</style>
